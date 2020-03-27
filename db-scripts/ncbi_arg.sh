bcc8c9de7118afc40e1b419bd6622d208d065948
Maxime Deraspe <maximilien1er@gmail.com>
2020-03-27 17:18:28 -0400

    change indentation

--- db-scripts/ncbi_arg.sh
+++ db-scripts/ncbi_arg.sh
@@ -8,7 +8,7 @@
 function download_files() {
 
     release=$(date +%Y-%m-%d)
-	script_path=`dirname $0`
+    script_path=`dirname $0`
 
     outdir=""
     if [ -z $1 ]
@@ -20,15 +20,15 @@ function download_files() {
     mkdir -p $outdir && cd $outdir
 
     echo -ne "Downloading NCBI ARG Reference file $release [$outdir].."
-	wget --quiet https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/Data/latest/ReferenceGeneCatalog.txt
+    wget --quiet https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/Data/latest/ReferenceGeneCatalog.txt
     # python $script_path/ncbi_entrez.py bioproject_nucccore 313047 gb > NCBI-ARG.gbk
-	echo " Done!"
+    echo " Done!"
 }
 
 # gunzip files
 function organize_files() {
-	echo -ne "Extracting CDS sequences from NCBI ..."
-	script_path=`dirname $0`
+    echo -ne "Extracting CDS sequences from NCBI ..."
+    script_path=`dirname $0`
     python $script_path/ncbi_arg.py ReferenceGeneCatalog.txt ncbi_arg.fasta
     echo " Done!"
 }
